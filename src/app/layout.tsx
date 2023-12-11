ad493ea5eaccd5d701ed4ddcc2a21b729b64b74c
Ricardo Malta <125774953+Ricardomms10@users.noreply.github.com>
2023-12-11 10:47:10 -0300

    links

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ricardo Malta Dev',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
+  )
+}
