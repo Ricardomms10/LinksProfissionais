9586914e5432463832c9a6e9fc533bfc9a37bd4b
Ricardo Malta <125774953+Ricardomms10@users.noreply.github.com>
2023-12-11 11:00:20 -0300

    likedin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <p>Web Developer</p>
 
       <div className={styles.boxBtns}>
-        <a href="https://www.linkedin.com/seu-link-do-linkedin" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/ricardo-malta/" target="_blank" rel="noopener noreferrer">
           <button className={styles.button}>
             <div className={styles.buttonContent}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin">
